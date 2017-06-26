8d0777cd5e8ffd505f3226e076c750f2259bda4e
Lenz <cle@horaios.local>
2017-06-26 23:13:07 +0200

    20170626-5

--- iBISS TM1/iBIS-TM1-Logging_Functions.ps1
+++ iBISS TM1/iBIS-TM1-Logging_Functions.ps1
@@ -10,24 +10,14 @@ function Start-iBISSTM1Log (
         }
 
         Process{
-            Add-Content -Path $Path -Value "################################################################################################"
+            Add-Content -Path $Path -Value "************************************************************************************************"
             Add-Content -Path $Path -Value " Started processing at [$([DateTime]::Now)]."
-            Add-Content -Path $Path -Value "################################################################################################"
+            Add-Content -Path $Path -Value "************************************************************************************************"
             Add-Content -Path $Path -Value ""
             Add-Content -Path $Path -Value " Running script task [$Task]."
             Add-Content -Path $Path -Value ""
-            Add-Content -Path $Path -Value "################################################################################################"
+            Add-Content -Path $Path -Value "************************************************************************************************"
             Add-Content -Path $Path -Value ""
-
-            #Write to screen for debug mode
-            Write-Debug -Path $Path -Value "################################################################################################"
-            Write-Debug -Path $Path -Value " Started processing at [$([DateTime]::Now)]."
-            Write-Debug -Path $Path -Value "################################################################################################"
-            Write-Debug -Path $Path -Value ""
-            Write-Debug -Path $Path -Value " Running script task [$Task]."
-            Write-Debug -Path $Path -Value ""
-            Write-Debug -Path $Path -Value "################################################################################################"
-            Write-Debug -Path $Path -Value ""
         }
 
         End{
