dc73c49d2ae4b2711dc237262a4041c2367db2d0
carslen <carslen@emea.corpdir.net>
2017-07-03 10:15:28 +0200

    Added code to onlinebackup subfunction

--- iBISS TM1/iBISS-TM1-Functions.ps1
+++ iBISS TM1/iBISS-TM1-Functions.ps1
@@ -41,7 +41,7 @@ function Start-iBISSTM1Log (
             Add-Content -Path $Path -Value "#"
             Add-Content -Path $Path -Value "################################################################################"
             Add-Content -Path $Path -Value ""
-            Add-Content -Path $Path -Value " Running script task $Task for instance ""$InstanceName""."
+            Add-Content -Path $Path -Value "  Running script task $Task"
             Add-Content -Path $Path -Value ""
             Add-Content -Path $Path -Value "################################################################################"
             Add-Content -Path $Path -Value ""
@@ -96,7 +96,7 @@ function Stop-iBISSTM1Log (
             Add-Content -Path $Path -Value "#"
             Add-Content -Path $Path -Value "################################################################################"
             Add-Content -Path $Path -Value ""
-            Add-Content -Path $Path -Value " Finished script task $Task for instance ""$InstanceName""."
+            Add-Content -Path $Path -Value "  Finished script task $Task"
             Add-Content -Path $Path -Value ""
             Add-Content -Path $Path -Value "################################################################################"
         }
@@ -289,7 +289,84 @@ function Start-iBISSTM1Backup (
                 }
             }
             elseif ($Type -eq "Offline") {
-                7z a -r $Target $Source >> $log                                     # Offline verwendet andere 7z Opts
+                Write-iBISSTM1Log -Path $log -Message "Starting TM1 $Task"
+                Write-iBISSTM1Log -Path $log -Message "Checking TM1 Service for $InstanceName"
+
+                # Checking Instance status (runnung, or not)
+                #
+                if ((Get-Service -Name "$InstanceName").Status -eq "Running") {
+                    Write-iBISSTM1Log -Path $log -Message "$InstanceName service is running, stopping..."
+                    Stop-Service -Name $InstanceName
+                    Write-iBISSTM1Log -Path $log -Message "Waiting for $InstanceName to be stopped. "
+                    while ((Get-Service -Name "$InstanceName").Status -eq "Running") {
+                        Write-iBISSTM1Log -Path $log -Message ". "
+                        Start-Sleep -Seconds 3
+                    }
+                    Write-iBISSTM1Log -Path $log -Message "$InstanceName stopped."
+                    Write-iBISSTM1Log -Path $log -Message "Creating backup container"
+                    
+                # Start creating the zip for backup
+                #
+                    7z a -tzip -r $Target $Source -bso1 > $env:TEMP\7ztemp.log
+                    
+                # Logging workaraound
+                #
+                    Add-Content -Path $log -Value (Get-Content $env:TEMP\7ztemp.log)
+                    Remove-Item -Path "$env:TEMP\7ztemp.log"
+                    
+                # Exitcodes for zip creation
+                #
+                    if ($LASTEXITCODE -eq "0") {
+                    Add-Content -Path $log -Value ""
+                    Write-iBISSTM1Log -Path $log -Message "Backup of $Source completed successful."
+                    }
+                    elseif ($LASTEXITCODE -eq "1") {
+                        Add-Content -Path $log -Value ""
+                        Write-iBISSTM1Warn -Path $log -Message "Backup of $Source completed with warnings."
+                    }
+                    elseif ($LASTEXITCODE -eq "2") {
+                        Add-Content -Path $log -Value ""
+                        Write-iBISSTM1ERROR -Path $log -Message "Backup of $Source failed!"
+                    }
+                
+                # Start instance after backup
+                # 
+                    Write-iBISSTM1Log -Path $log -Message "Starting $InstanceName service"
+                    Start-Service -Name $InstanceName
+                    if ((Get-Service -Name "$InstanceName").Status -eq "Running") {
+                        Write-iBISSTM1Log -Path $log -Message "$InstanceName started successful."
+                    }
+                    
+                }
+                else {
+                    Write-iBISSTM1Warn -Path $log -Message "Instance $InstanceName already stopped"
+                    Write-iBISSTM1Log -Path $log -Message "Creating backup container"
+                    
+                # Start creating the zip for backup
+                #
+                    7z a -tzip -r $Target $Source -bso1 > $env:TEMP\7ztemp.log
+                    
+                # Logging workaraound
+                #
+                    Add-Content -Path $log -Value (Get-Content $env:TEMP\7ztemp.log)
+                    Remove-Item -Path "$env:TEMP\7ztemp.log"
+                    
+                # Exitcodes for zip creation
+                #
+                    if ($LASTEXITCODE -eq "0") {
+                    Add-Content -Path $log -Value ""
+                    Write-iBISSTM1Log -Path $log -Message "Backup of $Source completed successful."
+                    }
+                    elseif ($LASTEXITCODE -eq "1") {
+                        Add-Content -Path $log -Value ""
+                        Write-iBISSTM1Warn -Path $log -Message "Backup of $Source completed with warnings."
+                    }
+                    elseif ($LASTEXITCODE -eq "2") {
+                        Add-Content -Path $log -Value ""
+                        Write-iBISSTM1ERROR -Path $log -Message "Backup of $Source failed!"
+                    }
+                }
+                
             }
         }
 
